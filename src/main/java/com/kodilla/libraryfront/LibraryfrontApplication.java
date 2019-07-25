f2285a70864982538fb11b47daedd964a884b036
aradzikowska12 <aradzikowska12@gmail.com>
2019-07-25 21:40:42 +0200

    initial commit

---
+++ src/main/java/com/kodilla/libraryfront/LibraryfrontApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.libraryfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryfrontApplication.class, args);
+    }
+
+}
