0caa8b34b526fc7c5ee618d1b105c4a73d480cd6
aradzikowska12 <aradzikowska12@gmail.com>
2019-08-06 20:57:36 +0200

    task38

---
+++ src/main/java/com/kodilla/libraryfront/CreateNewAccount.java
@@ -0,0 +1,6 @@
+package com.kodilla.libraryfront;
+
+import com.vaadin.flow.component.Component;
+
+public class CreateNewAccount extends Component {
+}
---
+++ src/main/java/com/kodilla/libraryfront/NavigatorUI.java
@@ -0,0 +1,22 @@
+package com.kodilla.libraryfront;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinRequest;
+import java.util.Collection;
+
+
+/*public class NavigatorUI extends UI {
+
+    protected static final String MAINPAGE = "mainPage";
+
+    @Override
+    protected void start(VaadinRequest vaadinRequest) {
+        getPage().setTitle("ALEKSANDRA LIBRARY");
+
+        navigator = new Navigator(this,this);
+
+
+
+    }
+}
+*/
