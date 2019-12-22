8ca348d686a3277c1fec48c9e5567dc64ed5d716
aradzikowska12 <aradzikowska12@gmail.com>
2019-12-22 18:00:49 +0100

    task 52 trzy pierwsze metody

--- src/main/java/com/kodilla/libraryfront/client/LibraryBackendClient.java
+++ src/main/java/com/kodilla/libraryfront/client/LibraryBackendClient.java
@@ -1,9 +1,14 @@
 package com.kodilla.libraryfront.client;
 
 import com.kodilla.libraryfront.configuration.LibraryBackendConfigration;
+import com.kodilla.libraryfront.dto.BookDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class LibraryBackendClient {
@@ -13,6 +18,21 @@ public class LibraryBackendClient {
 
     @Autowired
     private LibraryBackendConfigration libraryBackendConfigration;
+
+    public List<BookDto> getAllBooks(){
+        BookDto[] boardResponse = restTemplate.getForObject(libraryBackendConfigration.getLibrarybackendEndpoint() + "/books",BookDto[].class);
+        return Stream.of(boardResponse).collect(Collectors.toList());
+    }
+
+    public List<BookDto> getBooksOfTheAuthor(String author){
+        BookDto[] boardResponse = restTemplate.getForObject(libraryBackendConfigration.getLibrarybackendEndpoint() +"/books/author/"+author,BookDto[].class);
+        return Stream.of(boardResponse).collect(Collectors.toList());
+    }
+
+    public List<BookDto> getBooksAvaiableToRent(boolean rented){
+        BookDto[] boardResponse = restTemplate.getForObject(libraryBackendConfigration.getLibrarybackendEndpoint() +"/books/rented/"+rented,BookDto[].class);
+        return Stream.of(boardResponse).collect(Collectors.toList());
+    }
 
 
 }
