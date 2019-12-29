6bfd2108d09192dffdcaa8c994654f22739db07f
aradzikowska12 <aradzikowska12@gmail.com>
2019-12-29 14:04:40 +0100

    task54 mainPage

--- src/main/java/com/kodilla/libraryfront/websides/MainPage.java
+++ src/main/java/com/kodilla/libraryfront/websides/MainPage.java
@@ -2,33 +2,48 @@ package com.kodilla.libraryfront.websides;
 
 import com.kodilla.libraryfront.client.LibraryBackendClient;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route
+@StyleSheet("webapp/styles/Background.css")
 public class MainPage extends VerticalLayout {
 
     private final LibraryBackendClient libraryBackendClient;
+    private VerticalLayout layoutOfPage = new VerticalLayout();
+    private HorizontalLayout startingButtons = new HorizontalLayout();
+    //private Image logo;
+    private Label myLibrary;
+    private Button logIn;
+    private Button createAccount;
 
     public MainPage(LibraryBackendClient libraryBackendClient){
         this.libraryBackendClient=libraryBackendClient;
 
-        setSizeFull();
+        add(layoutOfPage,startingButtons);
 
-        Label myLibrary = new Label("Aleksandra Library");
-        myLibrary.setHeight("700");
-        myLibrary.setWidth("400");
+        //Image logo = new Image("file:webapp/images/logo.jpg");
+        //addClassName("mainPage");
+        //setSizeFull();
 
-        Button createAccount = new Button("Create Account");
+        myLibrary = new Label("Aleksandra Library");
+        myLibrary.setHeight("15000");
+        myLibrary.setWidth("30000");
+        layoutOfPage.addComponentAsFirst(myLibrary);
+        layoutOfPage.add(startingButtons);
+        layoutOfPage.setHorizontalComponentAlignment(Alignment.CENTER,myLibrary,startingButtons);
+
+        createAccount = new Button("Create Account");
         createAccount.addClickListener(event -> {
             getUI().get().navigate(CreateNewAccount.class);
         });
-
         createAccount.setHeight("200");
         createAccount.setWidth("200");
 
-        Button logIn = new Button("Log In");
+        logIn = new Button("Log In");
         logIn.addClickListener(event -> {
             getUI().get().navigate(LogIn.class);
         });
@@ -36,10 +51,10 @@ public class MainPage extends VerticalLayout {
         logIn.setHeight("200");
         logIn.setWidth("200");
 
-        add(myLibrary,logIn,createAccount);
+        startingButtons.addComponentAtIndex(0,createAccount);
+        startingButtons.addComponentAtIndex(1,logIn);
 
     }
-
 
 }
 
