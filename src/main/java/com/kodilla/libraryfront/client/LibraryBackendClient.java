6851071d23b2eae97b80fb41cf0a0b7d02fa4b66
aradzikowska12 <aradzikowska12@gmail.com>
2019-12-31 14:18:09 +0100

    task54 LogIn

--- src/main/java/com/kodilla/libraryfront/client/LibraryBackendClient.java
+++ src/main/java/com/kodilla/libraryfront/client/LibraryBackendClient.java
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,7 +28,6 @@ public class LibraryBackendClient {
     private LibraryBackendConfigration libraryBackendConfigration;
 
     private HttpHeaders httpHeaders;
-
 
     public List<BookDto> getAllBooks(){
         BookDto[] boardResponse = restTemplate.getForObject(libraryBackendConfigration.getLibrarybackendEndpoint() + "/books/",BookDto[].class);
@@ -149,7 +149,7 @@ public class LibraryBackendClient {
         return respEntity.getBody();
     }
 
-    /*public ReaderDto getReaderByLoginData(String email,String password){
+    public ReaderDto getReaderByLoginData(String email,String password){
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("email", email);
@@ -161,7 +161,7 @@ public class LibraryBackendClient {
         ResponseEntity<ReaderDto> respEntity = restTemplate.exchange(url, HttpMethod.GET, entity,  ReaderDto.class);
         return Optional.ofNullable(respEntity.getBody()).orElse(new ReaderDto());
     }
-*/
+
     public ReaderDto getReaderById(Long readerId){
         return restTemplate.getForObject(libraryBackendConfigration.getLibrarybackendEndpoint() + "/reader/" + readerId, ReaderDto.class);
     }
