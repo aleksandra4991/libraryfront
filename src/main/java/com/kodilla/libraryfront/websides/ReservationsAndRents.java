1ddeb2d13cb96e36480ec99994b960002051aac9
aradzikowska12 <aradzikowska12@gmail.com>
2020-02-09 16:32:39 +0100

    task60

--- src/main/java/com/kodilla/libraryfront/websides/ReservationsAndRents.java
+++ src/main/java/com/kodilla/libraryfront/websides/ReservationsAndRents.java
@@ -4,8 +4,11 @@ import com.kodilla.libraryfront.client.LibraryBackendClient;
 import com.kodilla.libraryfront.dto.BookDto;
 import com.kodilla.libraryfront.dto.ReaderDto;
 import com.kodilla.libraryfront.dto.ReservationDto;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 import java.util.ArrayList;
@@ -20,8 +23,13 @@ public class ReservationsAndRents extends HorizontalLayout {
     private List<ReservationDto> doneReservations;
     private Grid<BookDto> yourBooks;
     private Grid<ReservationDto> yourReservations;
+    private Button deleteReservation;
+
+    private VerticalLayout layoutOfReservationsAndRents = new VerticalLayout();
 
     private String uid;
+    private Long reservationId;
+
 
     public ReservationsAndRents(LibraryBackendClient libraryBackendClient) {
         this.libraryBackendClient = libraryBackendClient;
@@ -30,6 +38,7 @@ public class ReservationsAndRents extends HorizontalLayout {
         doneReservations = new ArrayList<>();
         yourBooks = new Grid<>(BookDto.class);
         yourReservations = new Grid<>(ReservationDto.class);
+        deleteReservation = new Button();
 
         yourBooks.setItems(rentedBooks);
         yourBooks.addColumn(BookDto::getTitle).setHeader("Title");
@@ -40,9 +49,13 @@ public class ReservationsAndRents extends HorizontalLayout {
         yourReservations.setItems(doneReservations);
         yourReservations.addColumn(ReservationDto::getActive).setHeader("Active");
         yourReservations.addColumn(ReservationDto::getReservedBooks).setHeader("ReservedBooks");
+        deleteReservation.addClickListener(e->deleteSpecifiedReservation());
 
+        add(layoutOfReservationsAndRents);
+        layoutOfReservationsAndRents.add(yourBooks,yourReservations, (Component) deleteReservation);
         showReaderRentedBooks();
         showReaderReservations();
+        deleteSpecifiedReservation();
 
     }
 
@@ -54,6 +67,11 @@ public class ReservationsAndRents extends HorizontalLayout {
     public void showReaderReservations() {
         ReaderDto readerDto = libraryBackendClient.getReaderByUid(uid);
         yourReservations.setItems(libraryBackendClient.getBooksReservedByUseer(readerDto));
+    }
+
+    public void deleteSpecifiedReservation(){
+        ReservationDto reservationDto = libraryBackendClient.getSpecifiedByIdReservation(reservationId);
+        libraryBackendClient.deleteReservation(reservationDto.getReservationId());
     }
 
 }
